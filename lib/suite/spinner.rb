0d8b6c9295272ece7af6800723f25a90e86c497a
Quinn Mueller <mueller.quinn@gmail.com>
2011-12-22 17:29:06 -0700

    Changed serialization for pipes from json to marshal. Updated method options for spin times

--- lib/suite/spinner.rb
+++ lib/suite/spinner.rb
@@ -1,6 +1,5 @@
 # encoding: utf-8
 module Suite
-  require 'json'
   
   class Spinner
     attr_accessor :exit_status, :block_output, :ruby_error
@@ -26,6 +25,7 @@ module Suite
     def initialize( opts = {}, bash_command = '', &block)
       options = {
         spinner_string: "Spin... ",
+        spinner_characters: %W( | / - \\ ),
         spin_block: lambda {|iteration|
           system( 'echo "$(tput cuu 1)"' )
           spinner_character_index = iteration % @spinner_characters.length
@@ -39,9 +39,8 @@ module Suite
       }.merge(opts)
       
       # option changable
-      @spinner_characters = %W( | / - \\ )
+      @spinner_characters = options[:spinner_characters]
       @spinner_string = options[:spinner_string]
-      
       @spin_block = options[:spin_block]
       @final_block = options[:final_block]
       
@@ -55,11 +54,34 @@ module Suite
         @is_bash_command = true
       end
       
-      
-      #pipe set up
+      # pipe set up
       @read_pipe, @write_pipe = IO.pipe
       @spin_iteration = 0
-    end #initilize
+    end # initilize
+    
+    
+    def spin_until_done(opts = {})
+      options = {
+        wait_time: 0.1
+      }.merge(opts)
+      
+      if @is_bash_command
+        self.fork_command_block
+        else
+        self.fork_ruby_block
+      end
+      
+      @write_pipe.close
+      self.spin_for_current_process( {wait_time: options[:wait_time]} )
+      
+      @final_block.call
+      output_return = Marshal::load( @read_pipe.read )
+      
+      @block_output = output_return[:block_output]
+      @exit_status = output_return[:exit_status]
+      @ruby_error = output_return[:ruby_error]
+      return self
+    end
     
     
     def reset_state
@@ -67,24 +89,25 @@ module Suite
       @spin_iteration = 0
     end
     
+    ########## Utility Methods ###########
     
     def fork_ruby_block
       @process_ID = Process.fork do
         @read_pipe.close
-        write_string = {
+        hash_for_pipe = {
           exit_status: true
         }
         
         begin 
           block_value = @process_block.call
-        rescue => error_detail
-          write_string[:ruby_error] = error_detail
-          write_string[:exit_status] = false
+        rescue => error
+          hash_for_pipe[:ruby_error] = error
+          hash_for_pipe[:exit_status] = false
         end
         
-        write_string[:block_output] = block_value
+        hash_for_pipe[:block_output] = block_value
         
-        @write_pipe.write(write_string.to_json)
+        @write_pipe.write(Marshal::dump( hash_for_pipe ))
         @write_pipe.close
       end 
     end # fork_ruby_block
@@ -96,24 +119,32 @@ module Suite
         
         block_value = @process_block.call
         
-        write_string = {
+        hash_for_pipe = {
           exit_status: $?.success?,
           block_output: block_value
-        }.to_json
+        }
         
-        @write_pipe.write(write_string)
+        @write_pipe.write(Marshal::dump( hash_for_pipe ))
         @write_pipe.close
       end
-    end
+    end # fork_command_block
     
     
     def spin_for_current_process (opts = {} )
       options = {
-        time: 0.1
+        wait_time: 0.1,
+        max_time: -1
       }.merge(opts)
       
+      start_time = Time.now.to_f
       done = false
       until done
+        if options[:max_time] > 0
+          if ( Time.now.to_f - start_time ) >= options[:max_time]
+            break
+          end
+        end
+        
         begin
           Process.getpgid( @process_ID )
           done = false
@@ -123,41 +154,16 @@ module Suite
         
         @spin_block.call(@spin_iteration)
         
-        sleep(options[:time])
+        sleep(options[:wait_time])
         @spin_iteration += 1
       end
-    end
-    
-    
-    def spin_until_done(opts = {})
-      options = {
-        time: 0.1
-      }.merge(opts)
-      
-      if @is_bash_command
-        self.fork_command_block
-        else
-        self.fork_ruby_block
-      end
-      
-      @write_pipe.close
-      self.spin_for_current_process
-      
-      @final_block.call
-      output_return = JSON.parse(@read_pipe.read)
-      
-      @block_output = output_return["block_output"]
-      @exit_status = output_return["exit_status"]
-      @ruby_error = output_return["ruby_error"]
-      
-      return self
-    end
+    end # spin_for_current_process
   end
 end
 
 
 # example usage
-#spinner1 = Spinner.new(spinner_string: "Spin me!... ") do
+#spinner1 = Suite::Spinner.new(spinner_string: "Spin me!... ") do
 #    sleep(3)
 #    evil.cheese
 #end.spin_until_done
@@ -165,6 +171,8 @@ end
 #puts spinner1.block_output
 #puts spinner1.exit_status
 #puts spinner1.ruby_error
+#puts ''
+#p spinner1.ruby_error
 
 #spinner2 = Spinner.new({spinner_string: "Spin me!... "}, 'ls').spin_until_done
 
