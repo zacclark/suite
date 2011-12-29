299b733779f5805b787cae0d9d6231ab8703a7d1
Quinn Mueller <mueller.quinn@gmail.com>
2011-12-28 22:58:44 -0700

    Reduced Class complexity (removed several unneeded methods) Added useful process state properties Improved readability Updated tests

--- lib/suite/spinner.rb
+++ lib/suite/spinner.rb
@@ -1,27 +1,59 @@
 # encoding: utf-8
 module Suite
   
+  # class for running a block or shell command on a new process
+  #   default spin behavior is a stdout visual spinner while
+  #   the process is running
   class Spinner
     attr_accessor :exit_status, :block_output, :ruby_error,
                   :spin_block, :process_block, :final_block
     
+    # outputs
     @exit_status
     @block_output
     @ruby_error
     
+    # internal state
     @is_bash_command
     @spin_iteration
     @process_ID
-    @read_pipe
-    @write_pipe
-    
+    @started
+    @finished
     @spinner_string
     @spinner_characters
     
+    # communication
+    @read_pipe
+    @write_pipe
+    
+    # blocks
     @spin_block
     @process_block
     @final_block
     
+    
+    ########## Readonly Properties ###########
+    
+    def has_started
+      return @started
+    end
+    
+    def has_finished
+      if @finished # do we know?
+        return true
+      elseif @process_ID != nil # could have finished since last spin call
+        begin
+          Process.getpgid( @process_ID )
+          return false
+        rescue Errno::ESRCH
+          return @finished = true
+        end
+      else # don't have a process at this point, hasn't started
+        return false
+      end
+    end
+    
+    ########## State Methods ###########
     
     def initialize( opts = {}, bash_command = '', &block)
       options = {
@@ -39,16 +71,21 @@ module Suite
         }
       }.merge(opts)
       
-      # option changable
+      # set initial internal state
+      @spin_iteration = 0
+      @started = false
+      @finished = false
+      @process_ID = nil
       @spinner_characters = options[:spinner_characters]
       @spinner_string = options[:spinner_string]
+      
+      # set blocks
       @spin_block = options[:spin_block]
       @final_block = options[:final_block]
-      
       if bash_command == ''
         @process_block = block
         @is_bash_command = false
-        else
+      else
         @process_block = lambda {
           eval("`#{bash_command} 2>&1`")
         }
@@ -57,80 +94,73 @@ module Suite
       
       # pipe set up
       @read_pipe, @write_pipe = IO.pipe
-      @spin_iteration = 0
     end # initilize
     
-    # Spins the current thread until the spinner process is finished
-    # @spin_block will get called every opts[:wait_time] seconds
-    def spin_until_done(opts = {})
-      options = {
-        wait_time: 0.1
-      }.merge(opts)
-      
-      self.fork_block
-      
-      @write_pipe.close
-      self.spin_for_current_process( {wait_time: options[:wait_time]} )
-      
-      @final_block.call
-      output_return = Marshal::load( @read_pipe.read )
-      
-      @block_output = output_return[:block_output]
-      @exit_status = output_return[:exit_status]
-      @ruby_error = output_return[:ruby_error]
-      return self
-    end
     
     # reset object state so it can be run again
+    #  returns reset success (true/false)
     def reset_state
       begin
         Process.getpgid( @process_ID )
-        raise 'Process still running'
+        return false # process is still running if we get here
       rescue Errno::ESRCH
-        return false # rescue error and indicate reset failure
       end
       @read_pipe, @write_pipe = IO.pipe
       @spin_iteration = 0
+      @started = false
+      @finished = false
+      @process_ID = nil
       @block_output = nil
       @exit_status = nil
       @ruby_error = nil
-      return true # indicate reset success
+      return true
     end
     
-    ########## Utility Methods ###########
-
-    def fork_block
-      @process_ID = Process.fork do
-        @read_pipe.close
-        hash_for_pipe = {
-          exit_status: true
-        }
-        begin
-          block_value = @process_block.call
-        rescue => error
-          hash_for_pipe[:ruby_error] = error
-          hash_for_pipe[:exit_status] = false
+    ########## Run Methods ###########
+    
+    # starts the block process
+    def start
+      if !@started
+        @started = true
+        @process_ID = Process.fork do
+          @read_pipe.close
+          hash_for_pipe = {
+            exit_status: true
+          }
+          begin
+            block_value = @process_block.call
+          rescue => error
+            hash_for_pipe[:ruby_error] = error
+            hash_for_pipe[:exit_status] = false
+          end
+          hash_for_pipe[:block_output] = block_value
+          
+          if @is_bash_command
+            hash_for_pipe[:exit_status] = $?.success?
+          end
+          @write_pipe.write(Marshal::dump( hash_for_pipe ))
+          @write_pipe.close
         end
-        hash_for_pipe[:block_output] = block_value
-        
-        if @is_bash_command
-          hash_for_pipe[:exit_status] = $?.success?
-        end
-        @write_pipe.write(Marshal::dump( hash_for_pipe ))
-        @write_pipe.close
-      end
-    end # fork_block
+        @write_pipe.close # close write pipe TO the new process so we don't hang
+      end # if !@started
+    end # start
     
     
-    def spin_for_current_process (opts = {} )
+    # Spins the current thread until the spinner process is finished 
+    #   or opts[:max_time] seconds have elapsed
+    # @spin_block will get called every opts[:wait_time] seconds
+    def spin (opts = {} )
       options = {
         wait_time: 0.1,
         max_time: -1
       }.merge(opts)
       
+      if !@started 
+        self.start
+      end
+      
       start_time = Time.now.to_f
-      done = false
-      until done
+      until @finished
         if options[:max_time] > 0
           if ( Time.now.to_f - start_time ) >= options[:max_time]
             break
@@ -138,33 +168,42 @@ module Suite
         end
         
         begin
-          Process.getpgid( @process_ID )
-          done = false
-          rescue Errno::ESRCH
-          done = true
+          Process.getpgid(@process_ID)
+          # the process is running, spin more
+          @spin_block.call(@spin_iteration)
+          sleep(options[:wait_time])
+          @spin_iteration += 1
+        rescue Errno::ESRCH
+          @finished = true
         end
+      end # until @finished
+      
+      if @finished
+        @final_block.call
+        output_return = Marshal::load( @read_pipe.read )
         
-        @spin_block.call(@spin_iteration)
-        
-        sleep(options[:wait_time])
-        @spin_iteration += 1
-      end # until done
-    end # spin_for_current_process
-  end
-end
+        @block_output = output_return[:block_output]
+        @exit_status = output_return[:exit_status]
+        @ruby_error = output_return[:ruby_error]
+      end # if done
+      
+      return self # convinience for calling new spinner in one line
+    end # spin
+  end # class Spinner
+end # module Suite
 
 
 # example usage
 #spinner1 = Suite::Spinner.new(spinner_string: "Spin me!... ") do
 #    sleep(3)
 #    evil.cheese
-#end.spin_until_done
+#end.spin
 #
 #puts spinner1.block_output
 #puts spinner1.exit_status
 #puts spinner1.ruby_error
 #puts ''
 #
-#spinner2 = Suite::Spinner.new({spinner_string: "Spin me!... "}, 'ls').spin_until_done
+#spinner2 = Suite::Spinner.new({spinner_string: "Spin me!... "}, 'ls').spin
 #
 #puts spinner2.exit_status
--- spec/suite/spinner_spec.rb
+++ spec/suite/spinner_spec.rb
@@ -7,39 +7,37 @@ describe Suite::Spinner do
       it "should contain exception description and failure status for invalid ruby" do
         spinner = Suite::Spinner.new do
           some_invalid_ruby_here_i_should_hope
-        end.spin_until_done
+        end.spin
+        puts 'hi'
         spinner.ruby_error.message.should include('undefined local variable or method `some_invalid_ruby_here_i_should_hope\' for ')
         spinner.exit_status.should == false
       end
       
-      it "should contain block data and failure status for invalid shell command"
+      it "should contain block data and failure status for invalid shell command" do
+        spinner = Suite::Spinner.new({},'cd jwiiwllsooithal').spin
+        spinner.block_output.should include('No such file or directory')
+        spinner.exit_status.should == false
+      end
     end # describe "Task failures"
     
     describe "Task success" do
       it "should contain success status for valid ruby" do
         spinner = Suite::Spinner.new do
           sleep(0.1)
-        end.spin_until_done
+        end.spin
         spinner.exit_status.should == true
       end
     end # describe "Task success"
   end # describe "Data returned from task"
   
   describe "" do
-    xit "should raise an exception if reset is attempted with process running" do
+    it "should indicate faliure if reset is attempted with process running" do
       spinner = Suite::Spinner.new do
         sleep(1)
       end
-      spinner.fork_block
-      exeption_caught = false
-      begin
-        spinner.reset_state
-      rescue
-        exeption_caught = true
-      ensure
-        sleep(1.1); # ugly way to prevent "Broken Socket" errors from cluttering test log
-      end
-      exeption_caught.should == true
+      spinner.start
+      spinner.reset_state.should == false
+      sleep(1.1); # ugly way to prevent "Broken Socket" errors from cluttering test log
     end # it "should raise an exception if reset is attempted with process running"
   end
 end
